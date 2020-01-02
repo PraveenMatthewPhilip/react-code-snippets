e3e897e1cb120462af3f6108d525dca247fd4ff5
PraveenMatthewPhilip <58952791+PraveenMatthewPhilip@users.noreply.github.com>
2020-01-02 16:10:22 +0530

    implemented setState with PrevoiusState

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import ClassCounter from './hooks/useState/CounterWithClass';
 import HookCounter from './hooks/useState/HookCounter';
+import HookCounterWithPreviousState from './hooks/useState/HookCounterWithPreviousState';
 
 function App() {
   return (
     <div className="App">
       <ClassCounter />
       <HookCounter />
+      <HookCounterWithPreviousState />
     </div>
   );
 }
---
+++ src/hooks/useState/HookCounterWithPreviousState.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const HookCounterWithPreviousState = () =>{
+
+    const [counter, setCounter] = useState(0);
+    const incrementBy5 = () => {
+        for(let i=0; i< 5; i++){
+            //setCounter(counter + 1)
+            setCounter(prevState => prevState + 1)
+        }
+    }
+    return(
+        <div>
+            Counter: {counter}
+            <button onClick={()=> setCounter(0)}>Reset</button>
+            <button onClick={()=> setCounter(prevState =>  prevState + 1)}>Increment</button>
+            <button onClick={()=> setCounter(prevState => prevState - 1)}>Decrement</button>
+            <button onClick={incrementBy5}>Increment by 5</button>
+        </div>
+    )
+}
+
+export default HookCounterWithPreviousState;
