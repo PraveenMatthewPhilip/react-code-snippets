f245f3ebd5354b83366d8229e3e41b5c71258701
PraveenMatthewPhilip <58952791+PraveenMatthewPhilip@users.noreply.github.com>
2020-01-03 12:33:17 +0530

    added feature to fetch postById.

--- src/hooks/useEffect/UseEffectDataFetching.js
+++ src/hooks/useEffect/UseEffectDataFetching.js
@@ -3,8 +3,13 @@ import axios from 'axios';
 
 const UseEffectDataFetching = () => {
 
-    const [ posts, setPosts ] = useState([])
+    const [ posts, setPosts ] = useState([]);
+    const [ post, setPost ] = useState({});
+    const [ Id, setId ] = useState(1);
+    const [IdFromButton, setIdFromButton] = useState(1);
+
     useEffect(() => {
+        console.log("useEffect - All Posts - UseEffectDataFetching.js")
         axios.get("https://jsonplaceholder.typicode.com/posts")
         .then(res => {
             setPosts(res.data);
@@ -12,8 +17,20 @@ const UseEffectDataFetching = () => {
         .catch(err => console.log(err));
     }, [])
 
+    useEffect(() => {
+        console.log("useEffect - Single Post - UseEffectDataFetching.js")
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${Id}`)
+        .then(res => {
+            setPost(res.data);
+        })
+        .catch(err => console.log(err));
+    }, [Id])
+
     return(
         <div>
+            <input type="text" value={Id} placeholder="Enter Post ID" onChange={ e => setId(e.target.value)} />
+
+            Single Post : {post.title}
             <ul>
                 {posts.map(post => <li key={post.id}>{post.title}</li>)}
             </ul>
